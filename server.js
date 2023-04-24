d54dd6be751b14a90cd758d49e9886d33784dc61
Thomas <tjb47.dev@gmail.com>
2023-04-23 22:15:10 -0400

    refactored server.js, switch mongoose connection

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
-const { connect, connection } = require('mongoose');
+const express = require('express');
+const mongoose = require('mongoose');
 
-const connectionString = 
-    process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialNetworkDB';
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-connect(connectionString, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use(require('./routes'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
-module.exports = connection;
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
+
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
